a6e121e6f44c4c5cdff11398defbad712b6e13ba
Jasmitsingh01 <jasmits007@gmail.com>
2024-05-26 11:30:39 +0530

    "updating Profile page"

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -5,7 +5,7 @@ function Profile() {
   return (
     <div className=" p-3">
         <h3 className=" text-2xl font-semibold mb-3">Profile</h3>
-        <div className="flex relative px-5 bg-blue-500 h-[200px] items-center justify-between">
+        <div className="flex relative px-5 bg-blue-500 h-[200px] items-center justify-between mb-[60px]">
             <img src="/download.jpeg"  className=" w-[100px] h-[100px] absolute bottom-[-30px] rounded-full" />
             <div className=" absolute bottom-[-80px]  h-[100px] w-full flex items-center justify-end px-10">
                 <Button classname=" text-blue-500 p-3   font-semibold border-blue-500 border-2 me-3 " types="button" Name=" upload Profile Image" />
@@ -14,7 +14,9 @@ function Profile() {
             </div>
         </div>
         <div>
-            <p></p>
+            <p className=" text-3xl font-semibold">Name</p>
+            <p className="mb-5">@username</p>
+
         </div>
     </div>
   )
